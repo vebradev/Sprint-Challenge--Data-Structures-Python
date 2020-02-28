c6d89d0e659531aa98a58ba783e087eb4e25ecf3
vebradev <justinas@vebra.dev>
2020-02-28 10:00:13 +0000

    add stretch goal

--- names/names.py
+++ names/names.py
@@ -11,13 +11,14 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+# STRETCH STARTS
+# set_1 = set(names_1)
+# set_2 = set(names_2)
+# duplicates = list(set_1.intersection(set_2))
+# STRETCH ENDS
 
 # create new tree and set the root value
 tree = bst(names_1[0])
@@ -41,3 +42,4 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
